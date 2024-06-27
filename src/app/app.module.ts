5d84653c1350edb7efae9576f997221a1daadbd9
eugenio-lk <39833534+eugenio-lk@users.noreply.github.com>
2024-06-27 19:20:35 +0200

    button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,26 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { PresentationComponent } from './component/presentation/presentation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalsPresentationComponent } from './component/modals/modals-presentation/modals-presentation.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, PresentationComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PresentationComponent,
+    ModalsPresentationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
